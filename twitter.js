304311c72096e7623c3f2f55ea7930d4165d17f3
sarasin <saracsin26@gmail.com>
2019-02-19 13:59:59 -0300

    versão 0.0.2 ok

--- twitter.js
+++ twitter.js
@@ -1,15 +1,20 @@
 let post;
 
-function disab() {
-    if(post == "") {
-    document.getElementById("button").disabled = true;
-    }
-    else if (post != "") {
-        document.getElementById("button").disabled = false;
-    }
-}
+document.getElementById("button").disabled = true;
 
 function text() {
     post = document.getElementById("tweet").value;
     document.getElementById("post").innerHTML = post;
+}
+
+function count() {
+    post = document.getElementById("tweet").value;
+    let count = (140 - post.length);
+    document.getElementById("count").innerHTML = count;
+    if(post.length == 0) {
+        document.getElementById("button").disabled = true;
+    }
+    else{
+        document.getElementById("button").disabled = false;
+    }
 }
